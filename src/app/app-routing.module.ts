91cc778138d3d626a19ef962e1aac10d454a5074
Sandeep Jadav <sandeepjada456@gmail.com>
2020-08-27 23:53:02 +0530

    chai project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,54 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'branches',
+    loadChildren: () => import('./branches/branches.module').then( m => m.BranchesPageModule)
+  },
+  {
+    path: 'branches',
+    loadChildren: () => import('./branches/branches.module').then( m => m.BranchesPageModule)
+  },
+  {
+    path: 'contact',
+    loadChildren: () => import('./contact/contact.module').then( m => m.ContactPageModule)
+  },
+  {
+    path: 'franchise',
+    loadChildren: () => import('./franchise/franchise.module').then( m => m.FranchisePageModule)
+  },
+  {
+    path: 'quality',
+    loadChildren: () => import('./quality/quality.module').then( m => m.QualityPageModule)
+  },
+  {
+    path: 'media',
+    loadChildren: () => import('./media/media.module').then( m => m.MediaPageModule)
+  },
+  {
+    path: 'language',
+    loadChildren: () => import('./language/language.module').then( m => m.LanguagePageModule)
+  },
+  {
+    path: 'about',
+    loadChildren: () => import('./about/about.module').then( m => m.AboutPageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
